0c884459af28cc74acd074bd8bbe7bc0b158392b
Programador-Kauan <kauanevandrof1@gmail.com>
2025-06-04 12:09:21 -0300

    algo

---
+++ exercicio2.php
@@ -0,0 +1,17 @@
+<?php
+$nome = $_GET ['nm'];
+echo $nome."<br>";
+$textarea = $_GET ['recado'];
+echo $textarea."<br>";
+$genero = $_GET ['genero'];
+echo "Seu gênero é $genero<br>";
+$estado = $_GET ['uf'];
+echo "Você mora no estado de $estado<br>";
+$opcao[0]=isset($_GET['op']);
+$opcao[1]=isset($_GET['op1']);
+$opcao[2]=isset($_GET['op2']);
+echo"<br> Sua escolhas foram:<br>";
+if($opcao[0] == 1) echo "Cinema<br>";
+if($opcao[1] == 1) echo "Esporte<br>";
+if($opcao[2] == 1) echo "Jogo<br>";
+?>
